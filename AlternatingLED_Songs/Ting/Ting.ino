004bbde7f3ecd6e4b344fa1703488ca3ff2c972a
Andres Graterol <graterol.andres0@knights.ucf.edu>
2022-01-18 17:20:01 -0500

    Remade the changes lost

--- AlternatingLED_Songs/Ting/Ting.ino
+++ AlternatingLED_Songs/Ting/Ting.ino
@@ -1,40 +1,60 @@
 // This code is being developed for Anisan Husman 
 // by the sexy librarian Nichika and Andres :)
 // cum
+#include <SD.h>
+#define SD_ChipSelectPin 10
+#include <TMRpcm.h>
+#include <SPI.h>
 
-
+// Button/LED Code
 // constants won't change. They're used here to
 // set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+//const int ledPin =  13;      // the number of the LED pin
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
-void setup() {
+
+// AUDIO
+TMRpcm tmrpcm;
+
+void setup() 
+{
+  /*
   Serial.begin(9600);
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  */
+  tmrpcm.speakerPin=9;
+  Serial.begin(9600);
+  if(!SD.begin(SD_ChipSelectPin))
+  {
+    Serial.println("SD fail");
+    return;
+  }
+  tmrpcm.setVolume(6);
+  tmrpcm.play("test.wav");
 }
+
+// Loop serves to constantly poll the state of the button
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
   // Show the state of pushbutton on serial monitor
-  Serial.println(buttonState);
+  //Serial.println(buttonState);
   
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if (buttonState == HIGH) 
   {
     // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    delay(10000);
+    //digitalWrite(ledPin, HIGH);
+    //delay(10000);
   } 
   else 
   {
     // turn LED off:
-    digitalWrite(ledPin, LOW);
+    //digitalWrite(ledPin, LOW);
   }
-  // Added the delay so that we can see the output of button
-  //delay(100);
 }
